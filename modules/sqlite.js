1190ba85c205624e51c47e9de8c52b45d36d7490
dani2318 <70331791+dani2318@users.noreply.github.com>
2023-05-13 18:04:09 +0200

    Various edit

--- modules/sqlite.js
+++ modules/sqlite.js
@@ -1,36 +1,60 @@
-const sqlite3= require("sqlite3").verbose();
-module.exports = class GC_Database{
-    db
-    constructor(path){
-        this.db = new sqlite3.Database(`${path}`, sqlite3.OPEN_READWRITE,(err)=>{
-            if(err) return console.error(err.message);
-        })
-        
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+module.exports = class GC_Database {
+    db;
+    constructor(path) {
+        this.db = new sqlite3.Database(
+            `${path}`,
+            sqlite3.OPEN_READWRITE,
+            (err) => {
+                if (err) return console.error(err.message);
+            }
+        );
     }
-    createTable(table, params){
-        let query = `CREATE TABLE ${table}(`
+    createTable(table, params) {
+        let query = `CREATE TABLE ${table}(`;
         params.forEach((param) => {
-            query +=`\n${param.name} ${param.type} ${(param.primary)? 'PRIMARY KEY': ''}${(param.notNull)? 'not null': ''} ${param.check},`;
+            query += `\n${param.name} ${param.type} ${
+                param.primary ? "PRIMARY KEY" : ""
+            }${param.notNull ? "not null" : ""} ${param.check},`;
         });
-        (query.endsWith(",")? query = query.slice(0,query.length-1) : query);
-        query+=`\n)`;
-        
-        console.log(query);
-        this.db.run(query, (err)=>{
-            if(err) return console.error(err.message)
-        });
-    }
-    insertIntoTable(table, params, values){
+        query.endsWith(",")
+            ? (query = query.slice(0, query.length - 1))
+            : query;
+        query += `\n)`;
 
+        console.log(query);
+        this.db.run(query, (err) => {
+            if (err) return console.error(err.message);
+        });
     }
-    readTable(table){
-        sql = `SELECT * FROM ${table}`
-        db.all(sql,(err,rows)=>{
-            if(err) return console.error(err.message)
-            rows.forEach((row)=>{
-                console.log(row)
-            })
-        })
+    insertIntoTable(table, params, values) {
+        let query = `INSERT INTO ${table}(`;
+        params.forEach((param) => {
+            query += `${param},`;
+        });
+        query.endsWith(",")
+            ? (query = query.slice(0, query.length - 1))
+            : query;
+        query += `) VALUES(`;
+        values.forEach((value) => {
+            query += `?,`;
+        });
+        query.endsWith(",")
+            ? (query = query.slice(0, query.length - 1))
+            : query;
+        query += `)`;
+        console.log(query);
+        this.db.run(query, values, (err) => {
+            if (err) return console.error(err.message);
+        });
+    }
+    readTable(table) {
+        let sql = `SELECT * FROM ${table}`;
+        this.db.all(sql, (err, rows) => {
+            if (err) return console.error(err.message);
+            console.log(rows);
+        });
     }
 
     /*
@@ -38,4 +62,4 @@ module.exports = class GC_Database{
     createTable("Users",
     [""])
     */
-}
+};
