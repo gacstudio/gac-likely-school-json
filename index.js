dfbb5a78217383b4318160cfceb6beec283767e2
pausellifrancesco <francypaus03@gmail.com>
2023-05-21 19:01:07 +0200

    Migliorata gestione
    Sistemata sessione, quindi login.
    Aggiunta registrazione funzionante.
    Piccoli problemi in visione profilo
    Mancano ancora post libri, generici, recupero

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 var server = require("http").createServer(app);
 
 app.use(session({
-    secret: 'il_tuo_segreto_di_sessione',
+    secret: 'ultrinus',
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -29,8 +29,8 @@ server.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
 
-
 //ROUTES
+
 app.get("/", (req, res) => {
     req.session.previousPage = req.originalUrl;
     if (req.session.isLoggedIn) {
@@ -39,37 +39,69 @@ app.get("/", (req, res) => {
         res.redirect('/login');
     }
 });
+
 app.get("/books", (req, res) => {
     req.session.previousPage = req.originalUrl;
-    res.render("books", { title: "Library" });
+    if (req.session.isLoggedIn) {
+        res.render("books", { title: "Library" });
+    } else {
+        res.redirect('/login');
+    }
 });
+
 app.get("/retrieval", (req, res) => {
     req.session.previousPage = req.originalUrl;
-    res.render("retrieval", { title: "Retrieval" });
+    if (req.session.isLoggedIn) {
+        res.render("retrieval", { title: "Retrieval" });
+    } else {
+        res.redirect('/login');
+    }
 });
+
 app.get("/tutors", (req, res) => {
     req.session.previousPage = req.originalUrl;
     readFile("./modules/config/users.json", (error, data) => {
         if (error) { console.log(error); return; }
         const users = JSON.parse(data);
         const usersOutput = users
-            .filter(user => user.ruolo === 'Tutor')
-            .map(user => {
-                const { password, ...userWithoutPassword } = user;
-                return userWithoutPassword;
-            });
-        res.render("tutors", { title: "Tutoring", users: usersOutput });
-    });
-});
+        .filter(user => user.role === "TUTOR")
+        .map(({ id, name, surname, username, email, role }) => ({ id, name, surname, username, email, role }));
+        if (req.session.isLoggedIn) {
+            res.render("tutors", { title: "Tutoring", users: usersOutput });
+        } else {
+            res.redirect('/login');
+        }});
+    });   
+
+    
 
 app.get("/profile", (req, res) => {
-    res.render("profile", { title: "Profile" });
+    console.log(req.query.id);
+    if (req.session.isLoggedIn) {
+        if(req.query.id){
+            readFile("./modules/config/users.json", (error, data) => {
+                if (error) { console.log(error); return; }
+                const users = JSON.parse(data);
+
+                if(user = users.find(user => user.id === req.query.id)){
+                    res.render("profile", { title: "Profile", user: users.find(user => user.id == req.query.id) });
+                }
+            });
+        } else{
+            res.render("profile", { title: "Profile", user: req.session.user });
+        }
+    } else {
+        res.redirect('/login');
+    }
 });
 
 app.get("/back", (req, res) => {
     res.redirect(req.session.previousPage || "/");
 });
-
+app.get("/logout", (req, res)=>{
+    req.session.destroy();
+    res.redirect("/login");
+})
 // session starting
 
 app.get("/login", (req, res) => {
@@ -83,14 +115,59 @@ app.post('/login', (req, res) => {
     readFile("./modules/config/users.json", (error, data) => {
         if (error) { console.log(error); return; }
         const users = JSON.parse(data);
-        console.log(users);
+        //console.log(users);
+        
+        /**
+         *     {
+        "id": 2,
+        "name": "Daniele",
+        "surname": "Giacchè",
+        "username": "dani2318",
+        "email": "mail@mail.it",
+        "password":"9125acdc004c33d61bb79666889492865dd58ace6fe30320422352d95ed77717",
+        "role": "ADMIN"
+    }
+]
+         */
+        
         if (checkCredentials(users, username, c_password)) {
+            user = users.find(us => us.username === username && us.password === c_password);
             req.session.isLoggedIn = true;
-            res.redirect('/');
+            req.session.user = user;
+            console.log(user); // True
+            res.redirect(`/`);
         } else {
             const message = 'Credenziali invalide';
-            res.render('login', { message });
+            res.render('login', {title: "Login", message });
         }
+    });
+});
+app.get('/register',(req, res)=>{
+    res.render('register', {title: "Register"});
+});
+
+app.post('/register',(req, res) => {
+    const { name, surname, username, email, password, role } = req.body;
+    var c_password = createHash("sha256").update(password).digest("hex");
+
+    readFile("./modules/config/users.json", (error, data) => {
+        if (error) { console.log(error); return; }
+        const users = JSON.parse(data);
+
+        users.push({
+            id: users.length+1,
+            name: name,
+            surname: surname,
+            username: username,
+            email: email,
+            password: c_password,
+            role: role
+        });
+        writeFile("./modules/config/users.json", JSON.stringify(users, null, 2), (err) => {
+            if (err) { console.log(err); return; }
+            res.redirect("/login", { title: "Login" });
+            //console.log("Updated file successfully");
+        });
     });
 });
 
